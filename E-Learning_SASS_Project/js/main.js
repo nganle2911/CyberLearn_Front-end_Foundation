f07bc43b5b0771fe0722fa3741fef79cba0e6196
Ngan LE <ngan.ltkn2911@gmail.com>
2022-04-15 15:32:34 +0200

    SASS for Courses - P3

---
+++ E-Learning_SASS_Project/js/main.js
@@ -0,0 +1,25 @@
+
+window.addEventListener("load", function() {
+    truncateCardTitle();
+});
+
+function truncateCardTitle() {
+    var cardList = document.getElementsByClassName("card-title");
+    console.log(cardList);
+
+    for (var i = 0; i < cardList.length; i++) {
+        var text = cardList[i].innerHTML; 
+        var newText = truncateString(text, 35); 
+        cardList[i].innerHTML = newText;
+    }
+}
+
+// To shorten the string 
+function truncateString(str, num) {
+    if (str.length > num) {
+        return str.slice(0, num) + "..."; 
+    } else {
+        return str; 
+    }
+}
+
